eef17d8411c11cafa63beb5c9a8ce4704baeacbc
adarshvdoit <adarsh.vdoit@gmail.com>
2023-06-29 20:12:30 +0530

    commit

--- app.py
+++ app.py
@@ -1,1 +1,72 @@
-from flask  import Flask
+from flask import Flask, render_template, request, redirect, session
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+app.secret_key = 'your-secret-key'  # Change this to a secure key
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'your-mysql-username'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'your-mysql-password'
+app.config['MYSQL_DATABASE_DB'] = 'your-mysql-database'
+app.config['MYSQL_DATABASE_HOST'] = 'your-mysql-host'
+mysql.init_app(app)
+
+
+@app.route('/')
+def home():
+    if 'email' in session:
+        email = session['email']
+        return render_template('home.html', email=email)
+    else:
+        return 'You are not logged in'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        user = cursor.fetchone()
+        
+        if user:
+            session['email'] = email
+            return redirect('/')
+        else:
+            return 'Invalid email or password'
+        
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+        
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        
+        cursor.execute("INSERT INTO users (email, name, password) VALUES (%s, %s, %s)", (email, name, password))
+        conn.commit()
+        
+        session['email'] = email
+        return redirect('/')
+        
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
